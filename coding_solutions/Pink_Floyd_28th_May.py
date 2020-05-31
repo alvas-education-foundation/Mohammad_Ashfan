b3514af77be5bf3e4dea3cdd5f6fbb9b43ea3ce5
MohammadAshfan12 <mohammadashfan48@gmail.com>
2020-05-31 08:23:01 -0700

    29/05/20

--- coding_solutions/Pink_Floyd_28th_May.py
+++ coding_solutions/Pink_Floyd_28th_May.py
@@ -5,8 +5,7 @@ records = []
 count = 0
 print('Enter the records')
 for i in range(0, n):
-    records.append(int(input()))
-    
+    records.append(int(input())) 
 for i in range(len(records)):
     if records[i] <= n:
         count ++
@@ -14,3 +13,4 @@ if count == n:
     print('Yes')
 else:
     print('No')
+
