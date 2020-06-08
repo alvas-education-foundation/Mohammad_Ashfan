9a1f15ca0abb788ba1487b7016f5879155d80582
MohammadAshfan12 <mohammadashfan48@gmail.com>
2020-06-08 06:02:35 -0700

    08/06/20

---
+++ coding_solutions/Decimal_to_Binary_8th_June.py
@@ -0,0 +1,14 @@
+# Function to print binary number using recursion
+
+def convertToBinary(n):
+   if n > 1:
+       convertToBinary(n//2)
+   print(n % 2,end = '')
+
+dec = 40
+
+convertToBinary(dec)
+print()
+
+
+
